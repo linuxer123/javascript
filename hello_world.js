b225de2bbb7209503c7ad48afe87acece9ca28d3
john <qq@qq.com>
2019-08-08 09:57:15 +0800

    first commit

---
+++ hello_world.js
@@ -0,0 +1,12 @@
+var username, greeting;
+greeting= "hello" +" "+ (username?username:"there");
+var o={x:3,y:2};
+delete o.x
+var a=[1,2,3];
+delete a[2];
+a[10]="a"
+for (var i in a) {
+	console.log(i);
+}
+console.log(a);
+// console.log(greeting);
